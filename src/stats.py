d82fb20995e39875744161eea516cddbcf8a38ed
shudong <dongs2011@gmail.com>
2025-01-11 07:47:58 -0800

    close the redis connection gracefully

--- src/stats.py
+++ src/stats.py
@@ -10,18 +10,21 @@ redis = Redis.from_url(os.getenv("REDIS_URL", "redis://localhost:6379/0"))
 
 
 async def run():
-    msg_pattern = f"{SERVICE_PREFIX}:chat:*:messages"
-    msg_keys = await redis.keys(msg_pattern)
-    for msg_key in msg_keys:
-        num_of_messages = await redis.llen(msg_key)
-        chat_id = str(msg_key).split(":")[2]
-        chat_info = await redis.get(chat_info_key(chat_id))
-        if chat_info:
-            chat_info = json.loads(chat_info)
-            chat_info["num_of_messages"] = num_of_messages
-            print(f"Info for {chat_id}: {chat_info}")
-        else:
-            print(f"No info for {chat_id}: messages={num_of_messages}")
+    try:
+        msg_pattern = f"{SERVICE_PREFIX}:chat:*:messages"
+        msg_keys = await redis.keys(msg_pattern)
+        for msg_key in msg_keys:
+            num_of_messages = await redis.llen(msg_key)
+            chat_id = str(msg_key).split(":")[2]
+            chat_info = await redis.get(chat_info_key(chat_id))
+            if chat_info:
+                chat_info = json.loads(chat_info)
+                chat_info["num_of_messages"] = num_of_messages
+                print(f"Info for {chat_id}: {chat_info}")
+            else:
+                print(f"No info for {chat_id}: messages={num_of_messages}")
+    finally:
+        await redis.close()  # Properly close Redis connection
 
 
 def main():
