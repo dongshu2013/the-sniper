4a6480c9777e9228c8bc6f8637e2a2c62ff80f67
shudong2013 <dongs2011@gmail.com>
2025-01-16 22:26:21 -0800

    simplify aggregator

--- src/aggregator.py
+++ src/aggregator.py
@@ -2,12 +2,8 @@ import asyncio
 import logging
 
 import asyncpg
-from redis.asyncio import Redis
 
-from src.common.config import DATABASE_URL, REDIS_URL
-from src.processors.msg_queue_processor import MessageQueueProcessor
-
-# from src.processors.score_summarizer import ChatScoreSummarizer
+from src.common.config import DATABASE_URL
 from src.processors.tg_link_importer import TgLinkImporter
 
 # Create logger instance
@@ -19,18 +15,13 @@ logger = logging.getLogger(__name__)
 
 
 async def run():
-    redis_client = Redis.from_url(REDIS_URL)
     pg_conn = await asyncpg.connect(DATABASE_URL)
 
     tg_link_importer = TgLinkImporter(pg_conn)
-    msg_queue_processor = MessageQueueProcessor(redis_client, pg_conn)
-    # summarizer = ChatScoreSummarizer(pg_conn)
 
     try:
         await asyncio.gather(
             tg_link_importer.start_processing(),
-            msg_queue_processor.start_processing(),
-            # summarizer.start_processing(),
         )
     except KeyboardInterrupt:
         logger.info("Shutting down...")
